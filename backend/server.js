9595205e07ac0c49c1305de39d674754c795c9c0
sumit <sumitmh567@gmail.com>
2022-12-19 20:21:32 +0530

    auth

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/db");
+const mongoose=require("mongoose");
 const app = express();
-dotenv.config();
+app.use(express.json());// to accept json data
 
+const userRoutes=require("./routes/userRoutes");
+
+mongoose.set('strictQuery', false);
+dotenv.config();
+connectDB();
 app.get("/",(req,res)=>{
     res.send("API is running");
 })
 
+app.use('/api/user',userRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,console.log(`server listening on ${PORT}`));
