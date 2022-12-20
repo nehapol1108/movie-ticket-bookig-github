a4177da41e24a4756f58c93b4fcbb7a477340a98
pratik <21pratikdhane@gmail.com>
2022-12-20 11:53:53 +0530

    pic

--- backend/models/seat.js
+++ backend/models/seat.js
@@ -4,12 +4,7 @@ const movieSchema = mongoose.Schema(
     moviename: { type: String, required: true },
     movietprice: { type: String, required: true },
     movietime: { type: String, required: true },
-    moviepic: {
-      type: String,
-      required: true,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    },
+    moviepic: { type: String, required: true },
   },
   { timestamps: true }
 );
