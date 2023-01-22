6e67b6c8b40c3f3b4d32f29143a12b16add740f2
nehapol <nehapol40@gmail.com>
2023-01-22 13:03:10 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const connectDB = require("./config/db");
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser')
 const cors = require("cors");
+const path = require("path")
+
+
 const app = express();
 app.use(express.json()); // to accept json data
 
@@ -20,10 +23,19 @@ app.get("/", (req, res) => {
 });
 
 app.use(cookieParser());
-app.use(cors({
-  origin:"http://localhost:3000"
-}));
+// app.use(cors({
+//   origin:"http://localhost:3000"
+// }));
+app.use(cors());
 app.use("/api/user", userRoutes);
 app.use("/api/movie", movieRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname,'../frontend/build')));
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
+})
+console.log(path.join(__dirname,"../frontend/build/index.html"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server listening on ${PORT}`));
