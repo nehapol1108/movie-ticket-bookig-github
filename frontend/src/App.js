12177ad7368f5dbdc8eb72ed979c25713f146fec
nehapol <nehapol40@gmail.com>
2023-03-15 12:30:29 +0530

    404 page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,33 @@
-import './App.css';
-import Homepage from './components/pages/Homepage';
-import MoviePage from './components/pages/MoviePage';
+import "./App.css";
+import Homepage from "./components/pages/Homepage";
+import MoviePage from "./components/pages/MoviePage";
 // import { HallForm } from './components/HallForm';
-import {Route} from "react-router-dom"
-import Moviebooking from './components/pages/Moviebooking';
-import SeatsBooked from './components/pages/SeatsBooked';
-import UserProfile from './components/UserProfile';
-import Error404 from './components/pages/Error404';
+import { Route } from "react-router-dom";
+import Moviebooking from "./components/pages/Moviebooking";
+import SeatsBooked from "./components/pages/SeatsBooked";
+import UserProfile from "./components/UserProfile";
+import Error404 from "./components/pages/Error404";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 function App() {
   return (
     <>
-  
-    <div className='App'>
-    <Route path="/" component={Homepage} exact/> 
-    </div>
-    <div>   
-    <Route path="/movies" component={MoviePage} /> 
-    </div>
-    <div>   
-    <Route path="/seat" component={Moviebooking}/> 
-    </div>
-    <div>   
-    <Route path="/booked" component={SeatsBooked}/> 
-    </div>
-    <div>   
-    <Route path="/profile" component={UserProfile}/> 
-    </div>
-    
-    {/* <div>   
-    <Route path='' component={Error404}></Route> 
-    </div>
-     */}
+      <Router>
+        <Switch>
+          <Route path="/" component={Homepage} exact />
+
+          <Route path="/movies" component={MoviePage} />
+
+          <Route path="/seat" component={Moviebooking} />
+
+          <Route path="/booked" component={SeatsBooked} />
+
+          <Route path="/profile" component={UserProfile} />
+
+          <Route path="*" component={Error404} />
+        </Switch>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {ChakraProvider} from "@chakra-ui/react";
-import {BrowserRouter as Router,Switch} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 // import MovieProvider from './context/MovieProvider';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    
-    <Router>
-        <Switch>
-    {/* <MovieProvider> */}
-    <ChakraProvider>
+  <ChakraProvider>
     <App />
-    </ChakraProvider>
-    </Switch>
-    {/* </MovieProvider> */}
-    </Router>
-    
-  
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
